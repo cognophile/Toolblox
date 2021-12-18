d7ee35dc3a515e85e3991a20b9b3fec1167fd1b9
cognophile <cognophile@users.noreply.github.com>
2021-12-18 12:30:57 +0000

    Initial project commit

---
+++ Toolblox.Tests/ToolbloxTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Cognophile.Toolblox.Tests
+{
+    public class ToolbloxTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
