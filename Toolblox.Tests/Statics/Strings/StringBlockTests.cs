aa011861c373e6d1f2353dcd7696d6b3bc340700
Alex <cognophile@users.noreply.github.com>
2021-12-30 17:44:57 +0000

    Update string block tests to cover missing cases and remove secondary covered test class

--- Toolblox.Tests/Statics/Strings/StringBlockTests.cs
+++ Toolblox.Tests/Statics/Strings/StringBlockTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Cognophile.Toolblox.Blocks.Statics.Strings;
 using NUnit.Framework;
 
@@ -24,7 +25,9 @@ namespace Toolblox.Tests.Statics.Strings
         [Test]
         public void IsNull_WhenArgumentIsNull_ThenBooleanTrueReturned()
         {
-            var actual = StringBlock.IsNull(null);
+            string subject = null;
+
+            var actual = StringBlock.IsNull(subject);
 
             Assert.IsTrue(actual);
         }
@@ -32,15 +35,19 @@ namespace Toolblox.Tests.Statics.Strings
         [Test]
         public void IsNull_WhenArgumentIsEmpty_ThenBooleanFalseReturned()
         {
-            var actual = StringBlock.IsNull("");
+            string subject = "";
+
+            var actual = StringBlock.IsNull(subject);
 
             Assert.IsFalse(actual);
         }
 
         [Test]
-        public void IsNull_WhenArgumentIsWhitespave_ThenBooleanFalseReturned()
+        public void IsNull_WhenArgumentIsWhitespace_ThenBooleanFalseReturned()
         {
-            var actual = StringBlock.IsNull("   ");
+            string subject = "   ";
+
+            var actual = StringBlock.IsNull(subject);
 
             Assert.IsFalse(actual);
         }
@@ -48,68 +55,539 @@ namespace Toolblox.Tests.Statics.Strings
         [Test]
         public void IsNull_WhenArgumentIsValid_ThenBooleanFalseReturned()
         {
-            var actual = StringBlock.IsNull("Lorem Ipsum");
+            string subject = "Lorem Ipsum";
+
+            var actual = StringBlock.IsNull(subject);
 
             Assert.IsFalse(actual);
         }
 
         [Test]
+        public void IsEmpty_WhenArgumentIsNotEmpty_ThenBooleanFalseReturned()
+        {
+            string subject = "Foobasr";
+
+            var actual = StringBlock.IsEmpty(subject);
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void IsEmpty_WhenArgumentIsWhitespace_ThenBooleanFalseReturned()
+        {
+            string subject = "   ";
+
+            var actual = StringBlock.IsEmpty(subject);
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void IsEmpty_WhenArgumentIsEmpty_ThenBooleanTrueReturned()
+        {
+            string subject = "";
+
+            var actual = StringBlock.IsEmpty(subject);
+
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void IsEmpty_WhenArgumentIsNull_ThenArgumentNullExceptionThrown()
+        {
+            string subject = null;
+
+            Assert.Throws<ArgumentNullException>(() => StringBlock.IsEmpty(subject));
+        }
+
+        [Test]
         public void IsNullOrEmpty_WhenArgumentIsNull_ThenBooleanTrueReturned()
         {
-            var actual = StringBlock.IsNullOrEmpty(null);
+            string subject = null;
+
+            var actual = StringBlock.IsNullOrEmpty(subject);
 
             Assert.IsTrue(actual);
         }
 
         public void IsNullOrEmpty_WhenArgumentIsEmpty_ThenBooleanTrueReturned()
         {
-            var actual = StringBlock.IsNullOrEmpty("");
+            string subject = "";
+
+            var actual = StringBlock.IsNullOrEmpty(subject);
 
             Assert.IsTrue(actual);
         }
 
         public void IsNullOrEmpty_WhenArgumentIsWhitespace_ThenBooleanTrueReturned()
         {
-            var actual = StringBlock.IsNullOrEmpty("   ");
+            string subject = "   ";
+
+            var actual = StringBlock.IsNullOrEmpty(subject);
 
             Assert.IsTrue(actual);
         }
 
-
         public void IsNullOrEmpty_WhenArgumentIsValid_ThenBooleanFalseReturned()
         {
-            var actual = StringBlock.IsNullOrEmpty("Lorem Ipsum");
+            string subject = "Lorem Ipsum";
+
+            var actual = StringBlock.IsNullOrEmpty(subject);
 
             Assert.IsFalse(actual);
         }
 
         [Test]
-        public void IsNullOrWhitespace_WhenArgumentIsNull_ThenBooleanTrueReturned()
+        public void IsWhitespace_WhenArgumentIsWhitespace_ThenBooleanTrueReturned()
         {
-            var actual = StringBlock.IsNullOrWhitespace(null);
+            string subject = "   ";
+
+            var actual = StringBlock.IsWhitespace(subject);
 
             Assert.IsTrue(actual);
         }
 
-        public void IsNullOrWhitespace_WhenArgumentIsEmpty_ThenBooleanTrueReturned()
+        [Test]
+        public void IsWhitespace_WhenArgumentIsNotWhitespace_ThenBooleanFalseReturned()
         {
-            var actual = StringBlock.IsNullOrWhitespace("");
+            string subject = "Foobar";
 
-            Assert.IsTrue(actual);
-        }
-
-        public void IsNullOrWhitespace_WhenArgumentIsWhitespace_ThenBooleanTrueReturned()
-        {
-            var actual = StringBlock.IsNullOrWhitespace("   ");
-
-            Assert.IsTrue(actual);
-        }
-
-        public void IsNullOrWhitespace_WhenArgumentIsWhitespace_ThenBooleanFalseReturned()
-        {
-            var actual = StringBlock.IsNullOrWhitespace("Lorem Ipsum");
+            var actual = StringBlock.IsWhitespace(subject);
 
             Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void IsWhitespace_WhenArgumentIsNull_ThenArgumentNullExceptionThrown()
+        {
+            string subject = null;
+
+            Assert.Throws<ArgumentNullException>(() => StringBlock.IsWhitespace(subject));
+        }
+
+        [Test]
+        public void IsNullOrWhitespace_WhenArgumentIsNull_ThenBooleanTrueReturned()
+        {
+            string subject = null;
+
+            var actual = StringBlock.IsNullOrWhitespace(subject);
+
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void IsNullOrWhitespace_WhenArgumentIsEmpty_ThenBooleanTrueReturned()
+        {
+            string subject = "";
+
+            var actual = StringBlock.IsNullOrWhitespace(subject);
+
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void IsNullOrWhitespace_WhenArgumentIsWhitespace_ThenBooleanTrueReturned()
+        {
+            string subject = "   ";
+
+            var actual = StringBlock.IsNullOrWhitespace(subject);
+
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void IsNullOrWhitespace_WhenArgumentIsWhitespace_ThenBooleanFalseReturned()
+        {
+            string subject = "Lorem Ipsum";
+
+            var actual = StringBlock.IsNullOrWhitespace(subject);
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void IsTitlecase_WhenArgumentFeaturesTitleCasing_ThenBooleanTrueReturned()
+        {
+            string subject = "Lorem Ipsum";
+
+            var actual = StringBlock.IsTitlecase(subject);
+
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void IsTitlecase_WhenArgumentDoesNotFeatureTitleCasing_ThenBooleanFalseReturned()
+        {
+            string subject = "Lorem ipsum";
+
+            var actual = StringBlock.IsTitlecase(subject);
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void IsTitlecase_WhenArgumentIsNull_ThenArgumentNullExceptionThrown()
+        {
+            string subject = null;
+
+            Assert.Throws<ArgumentNullException>(() => StringBlock.IsTitlecase(subject));
+        }
+
+        [Test]
+        public void IsTitlecase_WhenArgumentIsEmpty_ThenArgumentExceptionThrown()
+        {
+            string subject = "";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.IsTitlecase(subject));
+        }
+
+        [Test]
+        public void IsTitlecase_WhenArgumentIsWhitespace_ThenArgumentExceptionThrown()
+        {
+            string subject = "   ";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.IsTitlecase(subject));
+        }
+
+        [Test]
+        public void IsCapitalised_WhenArgumentHasCapitalisation_ThenBooleanTrueReturned()
+        {
+            string subject = "Lorem ipsum";
+
+            var actual = StringBlock.IsCapitalised(subject);
+
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void IsCapitalised_WhenArgumentHasNoCapitalisation_ThenBooleanFalseReturned()
+        {
+            string subject = "lorem ipsum";
+
+            var actual = StringBlock.IsCapitalised(subject);
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void IsCapitalised_WhenArgumentIsNull_ThenArgumentNullExceptionThrown()
+        {
+            string subject = null;
+
+            Assert.Throws<ArgumentNullException>(() => StringBlock.IsCapitalised(subject));
+        }
+
+        [Test]
+        public void IsCapitalised_WhenArgumentIsEmpty_ThenArgumentExceptionThrown()
+        {
+            string subject = "";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.IsCapitalised(subject));
+        }
+
+        [Test]
+        public void IsCapitalised_WhenArgumentIsWhitespace_ThenArgumentExceptionThrown()
+        {
+            string subject = "   ";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.IsCapitalised(subject));
+        }
+
+        [Test]
+        public void Matches_WhenArgumentMatchesGivenPattern_ThenBooleanTrueReturned()
+        {
+            string subject = "foo_bar";
+
+            var actual = StringBlock.Matches(subject, "([a-zA-Z]*)_([a-zA-Z]*)");
+
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void Matches_WhenArgumentDoesNotMatchGivenPattern_ThenBooleanFalseReturned()
+        {
+            string subject = "foobar";
+
+            var actual = StringBlock.Matches(subject, "([a-zA-Z]*)_([a-zA-Z]*)");
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void Matches_WhenPatternArgumentIsNull_ThenArgumentNullExceptionThrown()
+        {
+            string subject = "foo_bar";
+
+            Assert.Throws<ArgumentNullException>(() => StringBlock.Matches(subject, null));
+        }
+
+        [Test]
+        public void Matches_WhenPatternArgumentIsEmpty_ThenArgumentNullExceptionThrown()
+        {
+            string subject = "foo_bar";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.Matches(subject, ""));
+        }
+
+        [Test]
+        public void Matches_WhenPatternArgumentIsWhitespace_ThenArgumentNullExceptionThrown()
+        {
+            string subject = "foo_bar";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.Matches(subject, "   "));
+        }
+
+        [Test]
+        public void Matches_WhenArgumentIsNull_ThenArgumentNullExceptionThrown()
+        {
+            string subject = null;
+
+            Assert.Throws<ArgumentNullException>(() => StringBlock.Matches(subject, "([a-zA-Z]*)_([a-zA-Z]*)"));
+        }
+
+        [Test]
+        public void Matches_WhenArgumentIsEmpty_ThenArgumentExceptionThrown()
+        {
+            string subject = "";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.Matches(subject, "([a-zA-Z]*)_([a-zA-Z]*)"));
+        }
+
+        [Test]
+        public void Matches_WhenArgumentIsWhitespace_ThenArgumentExceptionThrown()
+        {
+            string subject = "   ";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.Matches(subject, "([a-zA-Z]*)_([a-zA-Z]*)"));
+        }
+
+        [Test]
+        public void Between_WhenArgumentIsNull_ThenArgumentNullExceptionThrown()
+        {
+            string subject = null;
+
+            Assert.Throws<ArgumentNullException>(() => StringBlock.Between(subject, "a", "b"));
+        }
+
+        [Test]
+        public void Between_WhenArgumentIsEmpty_ThenArgumentExceptionThrown()
+        {
+            string subject = "";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.Between(subject, "a", "b"));
+        }
+
+        [Test]
+        public void Between_WhenArgumentIsWhitespace_ThenArgumentExceptionThrown()
+        {
+            string subject = "   ";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.Between(subject, "a", "b"));
+        }
+
+        [Test]
+        public void Between_WhenLeftArgumentIsNull_ThenArgumentNullExceptionThrown()
+        {
+            string subject = "Here is a first test string";
+
+            Assert.Throws<ArgumentNullException>(() => StringBlock.Between(subject, null, "b"));
+        }
+
+        [Test]
+        public void Between_WhenLeftArgumentIsEmpty_ThenArgumentExceptionThrown()
+        {
+            string subject = "Here is a first test string";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.Between(subject, "", "b"));
+        }
+
+        [Test]
+        public void Between_WhenLeftArgumentIsWhitespace_ThenArgumentExceptionThrown()
+        {
+            string subject = "Here is a first test string";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.Between(subject, "   ", "b"));
+        }
+
+        [Test]
+        public void Between_WhenRightArgumentIsNull_ThenArgumentNullExceptionThrown()
+        {
+            string subject = "Here is a first test string";
+
+            Assert.Throws<ArgumentNullException>(() => StringBlock.Between(subject, "a", null));
+        }
+
+        [Test]
+        public void Between_WhenRightArgumentIsEmpty_ThenArgumentExceptionThrown()
+        {
+            string subject = "Here is a first test string";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.Between(subject, "a", ""));
+        }
+
+        [Test]
+        public void Between_WhenRightArgumentIsWhitespace_ThenArgumentExceptionThrown()
+        {
+            string subject = "Here is a first test string";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.Between(subject, "a", "   "));
+        }
+
+        [Test]
+        public void Between_WhenSubstringExistsInSource_ThenExpectedSubstringReturned()
+        {
+            string subject = "Here is a first test string";
+
+            var actual = StringBlock.Between(subject, "is", "test");
+
+            Assert.AreEqual("a first", actual);
+        }
+
+        [Test]
+        public void Between_WhenSubstringDoesNotExistInSource_ThenNullReturned()
+        {
+            string subject = "Here is a second test string";
+
+            var actual = StringBlock.Between(subject, "is", "first");
+
+            Assert.IsNull(actual);
+        }
+
+        [Test]
+        public void Explode_WhenArgumentIsNull_ThenArgumentNullExceptionThrown()
+        {
+            string subject = null;
+
+            Assert.Throws<ArgumentNullException>(() => StringBlock.Explode(subject, ' '));
+        }
+
+        [Test]
+        public void Explode_WhenArgumentIsEmpty_ThenArgumentExceptionThrown()
+        {
+            string subject = "";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.Explode(subject, ' '));
+        }
+
+        [Test]
+        public void Explode_WhenArgumentIsWhitespace_ThenArgumentExceptionThrown()
+        {
+            string subject = "   ";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.Explode(subject, ' '));
+        }
+
+        [Test]
+        public void Explode_WhenArgumentIsValid_ThenExpectedItemCountReturned()
+        {
+            string subject = "this is a string";
+
+            var actual = StringBlock.Explode(subject, ' ');
+
+            Assert.That(actual, Has.Count.EqualTo(4));
+        }
+
+        [Test]
+        public void Explode_WhenArgumentIsValid_ThenExpectedItemValuesReturned()
+        {
+            string subject = "this is a string";
+
+            var actual = StringBlock.Explode(subject, ' ') as List<string>;
+
+            Assert.AreEqual(0, actual.IndexOf("this"));
+            Assert.AreEqual(1, actual.IndexOf("is"));
+            Assert.AreEqual(2, actual.IndexOf("a"));
+            Assert.AreEqual(3, actual.IndexOf("string"));
+        }
+
+        [Test]
+        public void Titlecase_WhenArgumentIsNull_ThenArgumentNullExceptionThrown()
+        {
+            string subject = null;
+
+            Assert.Throws<ArgumentNullException>(() => StringBlock.Titlecase(subject));
+        }
+
+        [Test]
+        public void Titlecase_WhenArgumentIsEmpty_ThenArgumentExceptionThrown()
+        {
+            string subject = "";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.Titlecase(subject));
+        }
+
+        [Test]
+        public void Titlecase_WhenArgumentIsWhitespace_ThenArgumentExceptionThrown()
+        {
+            string subject = "   ";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.Titlecase(subject));
+        }
+
+        [Test]
+        public void Titlecase_WhenStringIsAlreadyInTitlecase_ThenOriginalStringReturned()
+        {
+            string subject = "This Is A String";
+
+            var actual = StringBlock.Titlecase(subject);
+
+            Assert.AreEqual(subject, actual);
+        }
+
+        [Test]
+        public void Titlecase_WhenStringIsNotInTitlecase_ThenMutatedStringReturned()
+        {
+            string subject = "this is a string";
+            string expected = "This Is A String";
+
+            var actual = StringBlock.Titlecase(subject);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Capitalise_WhenArgumentIsNull_ThenArgumentNullExceptionThrown()
+        {
+            string subject = null;
+
+            Assert.Throws<ArgumentNullException>(() => StringBlock.Capitalise(subject));
+        }
+
+        [Test]
+        public void Capitalise_WhenArgumentIsEmpty_ThenArgumentExceptionThrown()
+        {
+            string subject = "";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.Capitalise(subject));
+        }
+
+        [Test]
+        public void Capitalise_WhenArgumentIsWhitespace_ThenArgumentExceptionThrown()
+        {
+            string subject = "   ";
+
+            Assert.Throws<ArgumentException>(() => StringBlock.Capitalise(subject));
+        }
+
+        [Test]
+        public void Capitalise_WhenStringIsAlreadyCapitalised_ThenOriginalStringReturned()
+        {
+            string subject = "This is a string";
+
+            var actual = StringBlock.Capitalise(subject);
+
+            Assert.AreEqual(subject, actual);
+        }
+
+        [Test]
+        public void Capitalise_WhenStringIsNotCapitalised_ThenMutatedStringReturned()
+        {
+            string subject = "this is a string";
+            string expected = "This is a string";
+
+            var actual = StringBlock.Capitalise(subject);
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
